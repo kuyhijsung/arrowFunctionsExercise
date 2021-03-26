277372d2604cc0b036c932aa13d075346b0f9752
Andy <jihyuk.sung@gmail.com>
2021-03-26 10:46:18 -0600

    Finished

---
+++ main.js
@@ -0,0 +1,5 @@
+//ES2015 Arrow Functions Shorthand
+const double = arr => arr.map(val => val * 2);
+
+//Refractor all functions with arrow functions
+const squareAndFindEvens = numbers => numbers.map(num => num ** 2).filter(square => square % 2 === 0);
